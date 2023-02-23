46030d1b1629524247422a7e45d54956d25e7325
gwenparadis <gwendelynparadis2@gmail.com>
2023-02-23 17:42:56 -0500

    added info from previous portfolio

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,8 +3,9 @@ import React from "react";
 export default function About() {
   return (
     <div>
+      <img src="/Assets/profile-pic.jpg" alt="Profile" />
       <h1>About Me</h1>
-      <p>Paragraph about myself</p>
+      <p>Career, Personal Hobbies, etc.</p>
     </div>
   );
 }
