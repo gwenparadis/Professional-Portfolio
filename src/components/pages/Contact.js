9ead24600a3f2364d3732eb14ca065bc20042792
gwenparadis <gwendelynparadis2@gmail.com>
2023-02-23 16:56:36 -0500

    general file organization and desired tabs / pages

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>Larger and more obvious contact links -- form so they can email me?</p>
+    </div>
+  );
+}
---
+++ src/components/pages/Projects.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Blog() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <p>Clickable screenshots of projects and descriptions here</p>
+    </div>
+  );
+}
