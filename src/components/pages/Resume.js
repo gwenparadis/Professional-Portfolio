6e7768fe54770102864bdc93c34ecaf8d4bba244
gwenparadis <gwendelynparadis2@gmail.com>
2023-03-07 14:01:00 -0500

    added Resume

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,165 @@
+import React from "react";
+
+export default function Blog() {
+  return (
+    <div class="bg-dark text-white">
+      <h1 class="cover-container d-flex h-100 p-3 mx-auto flex-column text-center">
+        Professional Resume
+      </h1>
+
+      <div class="container p-2">
+        <div class="p-2 bg-info bg-opacity-10 border border-info border-start-0 rounded-end">
+          <h2>Current Employment</h2>
+        </div>
+      </div>
+
+      <div class="container p-2">
+        <div class="p-2">
+          <h2>Apprentice Software Engineer, JavaScript</h2>
+          <h6>
+            UNH Coding Bootcamp, Dover NH (Remote), September 2021 to present
+          </h6>
+          <h6>Field: Software Engineering</h6>
+        </div>
+
+        <div class="p-2">
+          <ul>
+            <li>
+              Built and deployed 18 individual mini-projects containing HTML,
+              CSS, and JavaScript elements
+            </li>
+            <li>
+              Utilized Agile Methodologies to collaborate with classmates to
+              create 3 separate projects
+            </li>
+            <li>
+              Maintained a 3.7 GPA or above for the entirety of the 24-week
+              bootcamp experience
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="container p-2">
+        <div class="p-2">
+          <h2>Sr Family Intervention Specialist</h2>
+          <h6>Youth Villages, Boston MA (Hyrbid), September 2021 to present</h6>
+          <h6>Field: Clinical Social Work </h6>
+        </div>
+
+        <div class="p-2">
+          <ul>
+            <li>
+              Elected to pioneer Boston’s second Intercept team, promoting
+              program expansion, November 2021
+            </li>
+            <li>
+              Earned “Boston’s Best” employee of the month award for exceptional
+              relationship building, January 2022
+            </li>
+            <li>
+              Earned “Staff Star” for Boston’s employee of the month for
+              exceptional case management, May 2022
+            </li>
+            <li>
+              Pioneered the per diem employment program for Specialists who are
+              continuing education, to increase job retention within the company
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="container p-2">
+        <div class="p-2 bg-info bg-opacity-10 border border-info border-start-0 rounded-end">
+          <h2>Previous Accomplishments</h2>
+        </div>
+      </div>
+
+      <div class="container p-2">
+        <div class="p-2">
+          <h2>Director of Remote Learning and Youth Programs</h2>
+          <h6>Laura Marie Howell, Newton MA, September 2020 to June 2021</h6>
+          <h6>Field: Education, Youth Sports and Recreation</h6>
+        </div>
+
+        <div class="p-2">
+          <ul>
+            <li>
+              Supported students in their remote learning, while each student
+              was promoted to next grade level
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="container p-2">
+        <div class="p-2">
+          <h2>Associate Aquatics Director</h2>
+          <h6>
+            Y of Greater Boston, Menino Branch, Boston MA, October 2019 to
+            September 2020
+          </h6>
+          <h6>
+            Field: Community Building and Management, Sports and Recreation
+          </h6>
+        </div>
+
+        <div class="p-2">
+          <ul>
+            <li>
+              Established a weekly “Staff Swim” hour to foster a healthy work
+              environment (up to 9 participants)
+            </li>
+            <li>
+              Instructed multiple cohorts as a Lifeguard Instructor, CPR and AED
+              Instructor, and First Aid Instructor
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="container p-2">
+        <div class="p-2">
+          <h2>Program Coordinator</h2>
+          <h6>
+            Boys & Girls Clubs of Southern Maine, South Portland ME, April 2018
+            to August 2019
+          </h6>
+          <h6>Field: Education, Youth Sports and Recreation</h6>
+        </div>
+
+        <div class="p-2">
+          <ul>
+            <li>
+              Created and implemented health and wellness programming for girls
+              ages 8-12
+            </li>
+            <li>
+              Pioneered a youth engineering experience, where youths would build
+              their own bikes to keep.
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="container p-2">
+        <div class="p-2 bg-info bg-opacity-10 border border-info border-start-0 rounded-end">
+          <h2>Education</h2>
+        </div>
+      </div>
+
+      <div class="container p-2">
+        <div class="p-2">
+          <h2>Certificate of Full Stack Web Development, JavaScript</h2>
+          <h6>
+            University of New Hampshire, Dover NH (Remote), Completed March 2023
+          </h6>
+        </div>
+        <div class="p-2">
+          <h2>Bachelors of Social Work, m. Cell Biology</h2>
+          <h6>University of Southern Maine, Portland ME, Completed May 2018</h6>
+        </div>
+      </div>
+    </div>
+  );
+}
