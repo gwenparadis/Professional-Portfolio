10eb8b93d6dfd13ba23749a9ed7c661bbbe2d293
gwenparadis <gwendelynparadis2@gmail.com>
2023-03-09 14:38:20 -0500

    working emailjs form and styling

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,35 +1,99 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
-export default function Contact() {
+const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_m8u9ifj",
+        "template_j0hader",
+        form.current,
+        "Io58VW2QLKZsm2d3C"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
-    <div class="bg-dark text-white p-5">
-      <h1 class="text-center p-2">Contact Me</h1>
-      <p class="p-2 text-center text-info">
-        Send a message by submitting the form below!
-      </p>
+    <div class="bg-dark text-white p-5 text-center">
+      <h1 class="p-2">Contact Information</h1>
+      <h5 class="p-2 text-info">
+        Connect with me by clicking on the icon of your preferred platform
+      </h5>
+      <small class="text-info">
+        These links can be found in the footer, as well.
+      </small>
+      <h5 class="p-2 text-center text-info">
+        If connecting on social media isn't your thing, Email me directly by
+        filling out the form below
+      </h5>
       <div class="container">
-        <form class="border border-info p-5 rounded">
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          class="form-control card flex-center border border-info p-5 rounded"
+        >
+          <h3 class="text-info text-center">Email Me</h3>
+          <small class="text-info mb-2">
+            I will only use this information to return your direct contact.
+          </small>
           <div class="form-group">
-            <label class="p-2">Email address</label>
+            <input
+              type="text"
+              class="form-control mb-2"
+              placeholder="Full Name"
+              name="user_name"
+              required
+            ></input>
+
             <input
               type="email"
               class="form-control"
-              placeholder="your email here"
+              placeholder="Preferred Email Address"
+              name="user_email"
+              required
             ></input>
           </div>
           <div class="form-group">
-            <label class="p-2">Message</label>
             <input
-              type="password"
-              class="form-control mb-4"
-              placeholder="your message here"
+              type="text"
+              class="form-control mt-2 mb-2"
+              placeholder="Message Subject"
+              name="subject"
+              required
             ></input>
           </div>
-          <button type="submit" class="btn btn-info">
-            Submit
+          <div class="form-group">
+            <textarea
+              class="form-control"
+              cols="30"
+              rows="10"
+              placeholder="Message"
+              name="message"
+              required
+            ></textarea>
+          </div>
+          <small class="p-2 text-info">
+            Pressing the button below will send your credentials and message
+            directly to the email inbox of Gwen Paradis
+          </small>
+          <button type="submit" class="btn btn-info p-2">
+            Send Message
           </button>
         </form>
       </div>
     </div>
   );
-}
+};
+
+export default Contact;
